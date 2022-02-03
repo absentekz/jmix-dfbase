2c38c200d2854297cc03764071999e19e545348a
absentekz <absente.kz@gmail.com>
2022-02-03 10:46:58 +0600

    1

---
+++ src/test/java/com/company/jmixdfbase/JmixDfbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.jmixdfbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmixDfbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
